c2e92106baaace35f250bc96ccfd0dbbb0b9f1a9
unknown <jeckseveg@gmail.com>
2024-06-18 23:18:04 -0400

    testing new ide

--- flask_app/app.py
+++ flask_app/app.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 
 # store ground points (causes errors)
 print('pickling')
-'''with open("static/data/chase_1/ground_points.pkl", "rb") as f:
-    ground_points = pickle.load(f)'''
+try:
+    with open("static/data/chase_1/ground_points.pkl", "rb") as f:
+        ground_points = pickle.load(f)
+except Exception as e:
+    print(f"Error loading ground points: {e}")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
