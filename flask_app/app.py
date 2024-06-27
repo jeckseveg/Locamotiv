ad6d299e1394a4e923bf7573ced5fa54b856df20
unknown <jeckseveg@gmail.com>
2024-06-27 19:21:08 -0400

    new pathing for videos

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,35 +7,6 @@ from utils import *
 from flask import Flask, jsonify, render_template, request, session
 
 app = Flask(__name__)
-
-# store ground points (causes errors)
-# print(os.listdir('static/data/chase_1/'))
-# try:
-#     with open("static/data/chase_1/ground_points.pkl", "rb") as f:
-#         ground_points = pickle.load(f)
-# except Exception as e:
-#     print(f"Error loading ground points: {e}")
-
-'''parser = argparse.ArgumentParser(description='Flask app for processing localization data')
-parser.add_argument('--on-local', action='store_true', help='Flag indicating if running on local')
-args = parser.parse_args()'''
-
-'''on_local = False#args.on_local  # Access the parsed flag
-print(on_local)'''
-
-'''if not on_local: 
-    assert os.environ.get("DATA_PATH"),"No environment variable set for DATA_PATH"
-    data_path = os.environ.get("DATA_PATH")
-    ground_points_path = f"{data_path}/chase_1/ground_points.json"
-    video_paths = {
-                    1:{"left":f"{data_path}/chase_1/sensor_1/video/left_quarter.mp4",
-                       "right":f"{data_path}/chase_1/sensor_1/video/right_quarter.mp4"},
-                    2:{"left":f"{data_path}/chase_1/sensor_2/video/left_quarter.mp4",
-                       "right":f"{data_path}/chase_1/sensor_2/video/right_quarter.mp4"},
-                    3:{"left":f"{data_path}/chase_1/sensor_3/video/left_quarter.mp4",
-                       "right":f"{data_path}/chase_1/sensor_3/video/right_quarter.mp4"},
-                    4:{"left":f"{data_path}/chase_1/sensor_4/video/left_quarter.mp4",
-                       "right":f"{data_path}/chase_1/sensor_4/video/right_quarter.mp4"}}'''
     
 #if on_local: 
 ground_points_path = "static/data/chase_1/ground_points.json"
